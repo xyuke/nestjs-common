2f9411d21d1c9d94941ad38d83fc5ed9c05a616f
xywait <734445114@qq.com>
2022-01-29 16:22:51 +0800

    Signed-off-by: xywait <734445114@qq.com>

---
+++ tools/gulp/tasks/clean.ts
@@ -0,0 +1,38 @@
+import { task, src, series } from "gulp";
+import { source } from "../config";
+import * as clean from "gulp-clean";
+import * as deleteEmpty from "delete-empty";
+
+/**
+ * Cleans the build output assets from the packages folders
+ */
+function cleanOutput() {
+	try{
+		return src(
+			[
+				`${source}/**/*.js`,
+				`${source}/**/*.d.ts`,
+				`${source}/**/*.js.map`,
+				`${source}/**/*.d.ts.map`,
+			],
+			{
+				read: false,
+			}
+		).pipe(clean());
+	}catch(error){
+		console.log("cleanOutput:error:", error)
+	}
+
+}
+
+/**
+ * Cleans empty dirs
+ */
+function cleanDirs(done: () => void) {
+	deleteEmpty.sync(`${source}/`);
+	done();
+}
+
+task("clean:output", cleanOutput);
+task("clean:dirs", cleanDirs);
+task("clean:bundle", series("clean:output", "clean:dirs"));
